04317e21dd8f7e553ae4b5fdd0813583bebfa882
Jonny Villarreal <ad.jonny.villarreal@unilatina.edu.co>
2026-06-10 14:54:11 -0500

    Debug variables entorno

--- backend/main.py
+++ backend/main.py
@@ -72,3 +72,14 @@ def test_db():
         "status": "conexion exitosa"
     }
 
+import os
+
+@app.get("/debug-env")
+def debug_env():
+    return {
+        "DB_HOST": os.getenv("DB_HOST"),
+        "DB_PORT": os.getenv("DB_PORT"),
+        "DB_NAME": os.getenv("DB_NAME"),
+        "DB_USER": os.getenv("DB_USER"),
+    }
+    
